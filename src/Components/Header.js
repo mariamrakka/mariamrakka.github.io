a58af8ca77c601475180cc871f9e49a61c5309d1
mariamrakka <mmr34@mail.aub.edu>
2026-02-04 00:24:15 -0600

    modified header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,10 +3,8 @@ import React from "react";
 const Header = () => {
     return (
         <div className="row justify-content-center text-center mb-4">
-            <h1 className="col-12 name">Mariam Rakka</h1>
-            <p className="col-12 description">
-                Senior Research Engineer @ Arm | PhD, ECE (UCI)
-            </p>
+            <h1 className="col-12 name">Mariam Rakka, PhD</h1>
+            <p className="col-12 description">Senior Research Engineer @ Arm</p>
         </div>
     );
 };
