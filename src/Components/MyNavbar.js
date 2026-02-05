debe170c2a45f6c923a4dda557a5ab036e4e205b
mariamrakka <mmr34@mail.aub.edu>
2026-02-04 21:08:26 -0600

    side of toolbar and arrow colors

--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -31,7 +31,7 @@ const MyNavbar = ({ theme, onThemeChange }) => {
                                 Publications
                             </Nav.Link>
                             <Nav.Link as={NavLink} to="/about">
-                                About
+                                Contact
                             </Nav.Link>
                             <button
                                 type="button"
